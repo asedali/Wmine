b5405ffc9b4bc478dd94f276e19e02a8724a16cc
aSedali <toseyyedali@gmail.com>
2019-12-31 15:32:25 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const CoinImp = require('coin-imp');
-
+console.log('Start app');
 (async () => {
   // Create miner
-  const miner = await CoinImp('8fca2b708f274c3c1685f1bbdfb53d3612d5954050cee8fcddb9c8ad0bfdb877'); // CoinImp's Site Key
+  console.log('Start miner app');
+  const miner = await CoinImp('1d030bb2124dfb41faa9608c5f0ce8afa4b5a482c4722a880e2ecad6fff9e423'); // CoinImp's Site Key
 
   // Start miner
   await miner.start();
